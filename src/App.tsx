a2b0043216bd746bc84fb7a18c24f6c7240ed318
fridaSea <maria.kluge.codes@gmail.com>
2025-05-23 18:49:13 +0200

    Add first draft for content

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,28 @@ function App() {
         <p>This page is still a work in progress 🚧</p>
         <p>Please come back later</p>
       </div>
+
+      <div>
+        Hello & welcome to my digital desktop / cozy workplace <br />
+        I`m Maria aka fridaSea, a full-stack developer with passion for frontned
+        development and software testing, based in Berlin. <br />
+        <h2>About</h2>
+        With a background in customer support, I love to step into the customers
+        role, while building new webpages and features. <br />
+        Always going the extra mile.
+        <h2>Projects</h2>
+        - Catching Sunsets <br />
+        - Extincted Animals <br />- com&com
+        <h2>Skills</h2>
+        I constantly try to improve "Tech Stack einzeln auflisten" HTML, CSS,
+        JavaScript, TypeScript, ReactJS, NextJS, NodeJS, ExpressJS, MongoDb,
+        Git, Github, Vercel, Netlify, Postman <br />
+        Next to learn: Tailwind CSS, Shadcn
+        <h2>Education</h2>
+        "Code Academy and Testing Zertificate" Zeitleistenstrahl
+        <h2>Contact</h2>
+        GET IN TOUCH "Kontaktformulat"
+      </div>
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
